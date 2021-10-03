c1c20bfb6280e436e6d13b77e5bbfb7f932b324d
EL2020 <el2020202020@gmail.com>
2021-10-03 18:23:13 -0400

    initial commit

---
+++ prestart.js
@@ -0,0 +1,23 @@
+Object.assign(sc.ASSIST_PUZZLE_SPEED, {
+    HIGH1: 1.1,
+    HIGH2: 1.2,
+    HIGH3: 1.3,
+    HIGH4: 1.4,
+    HIGH5: 1.5,
+});
+
+Object.assign(sc.ASSIST_ATTACK_FREQUENCY, {
+    HIGH1: 1.1,
+    HIGH2: 1.2,
+    HIGH3: 1.3,
+    HIGH4: 1.4,
+    HIGH5: 1.5,
+});
+
+Object.assign(sc.ASSIST_DAMAGE, {
+    HIGH1: 1.2,
+    HIGH2: 1.4,
+    HIGH3: 1.6,
+    HIGH4: 1.8,
+    HIGH5: 2,
+});
